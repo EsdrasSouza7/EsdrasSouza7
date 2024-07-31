b1485ea8a6a88a2d26ebb53d26af724f289316ce
Es7 <115493775+EsdrasSouza7@users.noreply.github.com>
2024-07-31 10:45:55 -0300

    Update serviceworker.js

--- serviceworker.js
+++ serviceworker.js
@@ -1,17 +1,21 @@
-var staticCacheName = "pwa_port";
+var staticCacheName = "pwa_dice";
 
 const filesToCache = [
 	'./index.html',
-	'./assets/css/main.css',
-	'./assets/js/main.js',
-  	'./assets/',
-	'./images/',
+	'./css/fontwasome-all.min.css',
+	'./css/main.css',
+	'./css/noscript.css',
+	'./js/breakpoints.min.js',
+	'./js/browser.min.js',
+	'./js/jquery.min.js',
+	'./js/main.js',
+	'./js/util.js',
+	'./images/'
 ];
  
 self.addEventListener("install", function (e) {
 	e.waitUntil(
 		caches.open(staticCacheName).then(function (cache) {
-			console.log('ios');
 			return cache.addAll(filesToCache);
 		})
 	);
